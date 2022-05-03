9eab362c2896aa96b72c801e0fc0b566c30c3ab9
krishnamoorthyH <krishnamoorthy2611@yahoo.co.in>
2022-05-03 12:32:29 +0530

    Update Test2.java

--- src/com/testPackage/Test2.java
+++ src/com/testPackage/Test2.java
@@ -5,6 +5,7 @@ public class Test2 {
 	public static void main(String[] args) {
 		
 		System.out.println("today is tuesday 03 May 2022");
+		System.out.println("today is tuesday 04 May 2022");
 	}
 	
 }
