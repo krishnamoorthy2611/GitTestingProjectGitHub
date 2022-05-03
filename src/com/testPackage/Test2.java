41a73f5f85d95b3dec7c1ecf586c2382d844bbaf
krishnamoorthy2611 <krishnamoorthy2611@yahoo.co.in>
2022-05-03 12:34:46 +0530

    added may in test2.java

--- src/com/testPackage/Test2.java
+++ src/com/testPackage/Test2.java
@@ -5,6 +5,7 @@ public class Test2 {
 	public static void main(String[] args) {
 		
 		System.out.println("today is tuesday 03 May 2022");
+		System.out.println("today is tuesday 06 May 2022");
 	}
 	
 }
